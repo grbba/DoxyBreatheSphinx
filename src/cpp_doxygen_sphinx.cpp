99b8f541f34f7bcfd26ba94a9bdbf1d6d86f5f35
Gregor Baues <grbaues@gmail.com>
2020-11-17 18:33:43 +0100

    more comments

--- src/cpp_doxygen_sphinx.cpp
+++ src/cpp_doxygen_sphinx.cpp
@@ -2,5 +2,6 @@
 #include "../include/cpp_doxygen_sphinx.hpp"
 
 void Foo::say_hello(std::string message) const {
-  std::cout << "Hello: " << message << std::endl;
+
+  std::cout << "Hello: " << message << std::endl; //!< some comments for this line
 }
