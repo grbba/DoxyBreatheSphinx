46170d6ed632254fd97f10bae476490dbb5d7105
Gregor Baues <grbaues@gmail.com>
2020-11-17 17:58:11 +0100

    Inital commit

---
+++ include/cpp_doxygen_sphinx.hpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+/// Foo class doing some foo
+
+class Foo {
+    /// Say Hello
+     /// @param message The message to print
+  void say_hello(std::string message) const;
+};
---
+++ src/cpp_doxygen_sphinx.cpp
@@ -0,0 +1,6 @@
+
+#include "../include/cpp_doxygen_sphinx.hpp"
+
+void Foo::say_hello(std::string message) const {
+  std::cout << "Hello: " << message << std::endl;
+}
